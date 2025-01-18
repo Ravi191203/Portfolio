e4afc6573d66263e047b169bfb2bd6e24648f9c2
RRGS DEV <raviraghavendrags@gmail.com>
2025-01-18 22:42:30 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,7 @@ import Particle from "../Particle";
 import portfolio from "../../Assets/Projects/portfolio.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 import DSMS from "../../Assets/Projects/DSMS.png";
+import js from "../../Assets/Projects/js.png";
 
 function Projects() {
   return (
@@ -17,7 +18,7 @@ function Projects() {
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
         </p>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        <Row style={{ justifyContent: "center", paddingBottom: "15px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={DSMS}
@@ -25,18 +26,17 @@ function Projects() {
               title="Dairy Serve Management System"
               description="The Dairy Serve Management System is a comprehensive solution designed to manage various aspects of a dairy business. It includes multiple modules and functionalities tailored to streamline operations, manage data efficiently, and provide an intuitive interface for different user roles."
               ghLink="https://github.com/Ravi191203/DAIRY-SERVE-MANAGEMENT-SYSTEM"
-              //demoLink="https://chatify-49.web.app/"
+              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={bitsOfCode}
-              isBlog={false}
+              isBlog={True}
               title="RRGS Tech Showcase"
               description="Explore my personal site, where I showcase my journey in computer science and web development. Dive into my projects, technical skills, and professional experiences, all curated to reflect my passion for technology and continuous learning."
               ghLink="https://sites.google.com/view/rrgscsstudent/home/"
-              //demoLink="https://blogs.soumya-jit.tech/"
             />
           </Col>
           <Col md={4} className="project-card">
@@ -46,10 +46,17 @@ function Projects() {
               title="Portfolio Website"
               description="Welcome to my portfolio website, crafted using React JS to showcase my skills, projects, and professional journey. Explore my work, learn about my background, and see how I blend creativity with code to build user-friendly and responsive applications. Whether you’re here to view my projects or connect with me, I hope my portfolio provides an insightful glimpse into my passion for technology and innovation."
               ghLink="https://sites.google.com/view/rrgscsstudent/home/"
-              //demoLink="https://blogs.soumya-jit.tech/"
             />
           </Col>
-          
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={js}
+              isBlog={True}
+              title="100Days Of JavaScript Challenge"
+              description="100Days Of JavaScript Challenge From Basic to Advance"
+              ghLink="https://100-days-100-js-projects-challenge.pages.dev/"
+            />
+          </Col>
         </Row>
       </Container>
     </Container>
